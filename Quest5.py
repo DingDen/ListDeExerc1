80f345f97cbe35ba220a7cac7f445e56fafbd681
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 15:56:04 -0300

    Create Quest5.py

---
+++ Quest5.py
@@ -0,0 +1,5 @@
+#Faça um Programa que converta metros para centímetros. 
+metros = float(input("Digite o valor em metros que você quer converter: "))
+centim = metros*10
+
+print("\tA conversão de ", metros, " metros para centímetros equivale a ", centim, " centímetros")
