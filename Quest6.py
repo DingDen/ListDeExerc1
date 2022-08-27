a93f862a4d22723200f641596356d79dfc3bbbd0
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 16:01:09 -0300

    Create Quest6.py

---
+++ Quest6.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+r = float(input("Insira o valor do raio do círculo: "))
+pi = 3.14
+area = pi*r
+
+print("A área do círculo equivale a ", area, " unidades de área (u.a)")
