1952aa9cceebafebdb9d70e2e40301c92e6b3ed5
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 16:10:57 -0300

    Create Quest7.py

---
+++ Quest7.py
@@ -0,0 +1,6 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+l = float(input("Digite o módulo do lado do quadrado: "))
+aq = l*l
+daq = 2*aq
+
+print("\tA área desse quadrado é ", aq, " ua"); print("\tO dobro da área desse quadrado equivale a ", daq, " ua")
