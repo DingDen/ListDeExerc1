5ee30fe418d0b1a1ad5acbcaeedbc884d9460330
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 15:41:23 -0300

    Create Quest4.py

---
+++ Quest4.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+n1 = float(input("Insira sua nota do primeiro bimestre:\n"))
+n2 = float(input("Insira sua nota do segundo bimestre:\n"))
+n3 = float(input("Insira sua nota do terceiro bimestre:\n"))
+n4 = float(input("Insira sua nota do quarto bimestre:\n"))
+
+media = (n1+n2+n3+n4)/4
+
+print("A sua média final é: ", media)
