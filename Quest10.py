d55c734828c4f70a5e6f45510a0496ecbee7b544
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 16:45:29 -0300

    Create Quest10.py

---
+++ Quest10.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+cel = float(input("Digite o valor da temperatura em graus Celsius: "))
+fah = 1.8*cel + 32
+
+print("A conversão de ", cel, " graus Celsius equivale a ", fah, " graus Fahrenheit")
