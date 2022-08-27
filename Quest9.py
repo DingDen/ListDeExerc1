a04aa80d389e37c2f456d0e49044aa0fd9a62e2f
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-27 16:48:48 -0300

    Update Quest9.py

--- Quest9.py
+++ Quest9.py
@@ -1,5 +1,5 @@
 #Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. C = 5 * ((F-32) / 9).
-fah = float(input("Digite o valor da temperatura em Fahrenheit: "))
+fah = float(input("Digite o valor da temperatura em graus Fahrenheit: "))
 cel = 5*((fah-32)/9)
 
 print("A conversão de ", fah, " graus Fahrenheit equivale a ", cel, " graus Celsius")
