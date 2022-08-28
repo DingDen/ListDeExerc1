519c7dcfc1f38dedefbc18f2ad3fcf206fdfe8a5
DingDen <87573646+DingDen@users.noreply.github.com>
2022-08-28 14:40:01 -0300

    Update Quest_17.py

--- Quest_17.py
+++ Quest_17.py
@@ -6,3 +6,24 @@
 #comprar apenas galões de 3,6 litros;
 #misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
 
+a = float(input("Digite o tamanho, em metros quadrados, da área a ser pintada: "))
+lit = a/6
+lata = int(lit/18)
+gal = int(lit/3.6)
+latagal = int(lit/23.76)
+
+if lit%18 != 0:
+	lata = lata+1
+	preco_tot_lata = lata*80
+  
+if lit%3.6 != 0:
+	gal = gal+1
+	preco_tot_gal = gal*25
+  
+if lit%23.76 != 0:     #Mistura = 18 + 3.6 + 10% de folga = 21.6 + 21.6*10% = 23.76 Litros 
+	latagal = latagal+1
+	preco_tot_latagal = latagal*105
+
+print("\tConsiderando apenas uma compra de latas de tinta, a quantidade de latas a serem compradas equivale a ", lata, f" e o preço total é R${preco_tot_lata:.2f}")
+print("\tConsiderando apenas uma compra de galões de tinta, a quantidade de galões a serem compradas equivale a ", gal, f" e o preço total é R${preco_tot_gal:.2f}")
+print("\tConsiderando uma mistura entre latas e galões, a quantidade dessa mistura a ser comprada equivale a ", latagal, f" e o preço total é R${preco_tot_latagal:.2f}")
